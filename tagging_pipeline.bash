bffbff2e6ab3acc40248505264605aa3ad07a093
Javi Corvi <javicorvi@gmail.com>
2018-09-14 11:15:49 +0200

    pipelindes draft

--- tagging_pipeline.bash
+++ tagging_pipeline.bash
@@ -1,4 +1,9 @@
 # Execute tagging chemspot dictionary update
 java -Xmx5G -jar chemspot.jar -u
 
-# Execute tagging chemspot 
+# Execute tagging chemspot abstracts
+java -jar /home/jcorvi/projects/chemical_compound_tagger/target/chemical_compound_tagger-0.0.1-SNAPSHOT-jar-with-dependencies.jar /home/jcorvi/text_tox_test/pubmed/chemical_compound_tagger.properties
+
+# Execute tagging chemspot sentences
+java -jar /home/jcorvi/projects/chemical_compound_tagger/target/chemical_compound_tagger-0.0.1-SNAPSHOT-jar-with-dependencies.jar /home/jcorvi/text_tox_test/pubmed/chemical_compound_tagger.properties
+
--- training_pipeline.bash
+++ training_pipeline.bash
@@ -1,14 +1,16 @@
+#sudo docker build -t sentence_splitting .
+
 # Execute Download Random Abstracts Retrieval Container
-sudo docker run --rm  -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data pubmed_random_retrieval python pubmed_random_retrieval.py -p /app/data/random_retrieval.properties
+sudo docker run --rm  -u $UID -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data pubmed_random_retrieval python pubmed_random_retrieval.py -p /app/data/random_retrieval.properties
 
 # Execute Download Gold Answer Retrieval Container
-sudo docker run --rm  -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data pubmed_goldsearch_retrieval python pubmed_goldsearch_retrieval.py -p /app/data/gold_retrieval.properties
+sudo docker run --rm  -u $UID -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data pubmed_goldsearch_retrieval python pubmed_goldsearch_retrieval.py -p /app/data/gold_retrieval.properties
 
 # Generate Training Data Set
-sudo docker run --rm  -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data generate_training_dataset python generate_training_dataset.py -p /app/data/generate_training_dataset.properties
+sudo docker run --rm  -u $UID -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data generate_training_dataset python generate_training_dataset.py -p /app/data/generate_training_dataset.properties
 
 # Execute sentences splitting on data set 
-sudo docker run --rm  -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data sentence_splitting python sentence_splitting.py -p /app/data/sentence_splitting.properties
+sudo docker run --rm -u $UID -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data sentence_splitting python sentence_splitting.py -p /app/data/sentence_splitting.properties
 
 # Generate Training Model
 java -jar /home/jcorvi/projects/text_classificator_model_training/target/text_classificator_training-0.0.1-SNAPSHOT-jar-with-dependencies.jar /home/jcorvi/text_tox_test/pubmed/classificator/text_classificator_model_training.properties
@@ -18,4 +20,4 @@ java -jar /home/jcorvi/projects/text_classificator_model_training/target/text_cl
 
 
 # Remove duplicated articles from random, if an article is in goldsearch is removed from random
-# sudo docker run --rm  -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data generate_training_dataset python remove_goldanswer_articles_from_random.py -p /app/data/#generate_training_dataset.properties
+# sudo docker run --rm -u $UID -v /home/jcorvi/text_tox_test/pubmed/classificator:/app/data generate_training_dataset python remove_goldanswer_articles_from_random.py -p /app/data/#generate_training_dataset.properties
